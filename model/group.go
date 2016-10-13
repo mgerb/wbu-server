c8a08eb03821d95f47d53bca53de093b9c9f40bb
Mitchell <mgerb42@gmail.com>
2016-10-12 23:43:36 -0500

    wip

--- model/group.go
+++ model/group.go
@@ -2,7 +2,9 @@ package model
 
 //schema
 const (
-    gr_keys = "u_keys" //increments on each new user
-    gr_name = "u_name:" //username maps to user id
-    group = "u_hash:" //hash map for users = id maps to key values
+	gr_keys    = "gr_keys"     //increments on each new group
+	gr_name    = "gr_name:"    //username maps to group id
+	gr_hash    = "gr_hash:"    //group id maps to hash map for group information
+	gr_members = "gr_members:" //set containing members for each group
+	gr_invites = "gr_invites"  //set containing members that currently have an invite to a group
 )
