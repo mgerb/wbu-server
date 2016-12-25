184a140ded268a6a136054c5913422bf2ba93aef
Mitchell <mgerb42@gmail.com>
2016-12-25 17:15:09 -0600

    use lua scripts now

--- operations/lua/lua.go
+++ operations/lua/lua.go
@@ -1,17 +1,34 @@
 package lua
 
 import (
-	"fmt"
 	"io/ioutil"
+	"log"
 )
 
-var File map[string]string
+var file = map[string]string{}
 
+//Init - reads all files in to a map of strings
 func Init() {
+	dir := "./operations/lua/scripts/"
 
-	files, _ := ioutil.ReadDir("./operations/lua/scripts")
+	files, _ := ioutil.ReadDir(dir)
+
+	var err error
+	var fileContents []byte
+
 	for _, f := range files {
-		fmt.Println(f.Name())
+		fileContents, err = ioutil.ReadFile(dir + f.Name())
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		file[f.Name()] = string(fileContents)
 	}
 
+}
+
+//Use - return lua script to use
+func Use(fileName string) string {
+	return file[fileName]
 }
